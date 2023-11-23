e38a1d74620f57a0a4106a30c89fa9c1e3b22f27
Adolligit <adelson.onod@gmail.com>
2023-11-23 15:07:46 -0300

    feat[migration]: (User) adicionando a biblioteca 'md5' para criar o hash da senha

--- back-end/src/database/seeders/20221028150655-users.js
+++ back-end/src/database/seeders/20221028150655-users.js
@@ -1,24 +1,26 @@
 'use strict';
 
+const md5 = require("md5");
+
 module.exports = {
   async up (queryInterface, _Sequelize) {
     await queryInterface.bulkInsert('users', [
       {
         name: 'Delivery App Admin',
         email: 'adm@deliveryapp.com',
-        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+        password: md5('--adm2@21!!--'),
         role: 'administrator',
       },
       {
         name: 'Fulana Pereira',
         email: 'fulana@deliveryapp.com',
-        password: '3c28d2b0881bf46457a853e0b07531c6',
+        password: md5('fulana@123'),
         role: 'seller',
       },
       {
         name: 'Cliente Zé Birita',
         email: 'zebirita@email.com',
-        password: '1c37466c159755ce1fa181bd247cb925',
+        password: md5('zebirita@123'),
         role: 'customer',
       },
     ], {});
